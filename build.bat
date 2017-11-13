a6caf12fa9e685ae2f7028e819d286405077e04b
osenouci <=>
2017-11-13 11:37:50 +0100

    Angular project update @ Mon 11/13/2017 - 11:37:50.66

--- build.bat
+++ build.bat
@@ -2,7 +2,18 @@
 set ngProjectDir=C:\Users\OthmaneSENOUCI\projects\robotStore
 set serverDir=C:\Users\OthmaneSENOUCI\projects\robotStoreServer
 
+rem Build the angular project
 cd %ngProjectDir%
 call call ng build --aot -prod
+
+rem Copy the dist directory to node server
+call xcopy  %ngProjectDir%\dist %serverDir%\dist /e/i/h/Y
+
+rem commit to git
 cd %serverDir%
+call git add .
+call git commit -m "Angular project update @ %DATE% - %TIME%"
+rem upload the server to the heroku cloud
+echo "Uploading to heroku"
+
 call git push heroku master
